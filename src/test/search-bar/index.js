e4e7b25daf7157193d5c4e08bd3639fadae717ff
Xiaojin Cai <xcai0511@gmail.com>
2023-04-11 14:08:34 -0400

    search bar margin

--- src/test/search-bar/index.js
+++ src/test/search-bar/index.js
@@ -8,8 +8,8 @@ const SearchBar = () => {
         <section className="wd-home">
             <div className="wd-overlay"></div>
             <div className="wd-searchbar container d-none d-md-block">
-                <h2>Search Your Events</h2>
-                <div className="row">
+                <h2 className="ms-3">Search Your Events</h2>
+                <div className="row ms-1 me-1">
                     <InputGroup className="mt-2">
                         <InputGroup.Text>
                             <i className="bi bi-geo-alt"></i>
@@ -24,20 +24,23 @@ const SearchBar = () => {
                 </div>
             </div>
             <div className="wd-searchbar container d-md-none">
-                <h2>Search Your Events</h2>
-                <InputGroup className="mt-2 mb-2 opacity-100">
-                    <InputGroup.Text>
-                        <i className="bi bi-geo-alt"></i>
-                    </InputGroup.Text>
-                    <input placeholder="City or Zip Code" className="d-inline form-control"/>
-                </InputGroup>
-                <InputGroup className="mb-2 opacity-100">
-                    <InputGroup.Text>
-                        <i className="bi bi-geo-alt"></i>
-                    </InputGroup.Text>
-                    <input placeholder="Search for events, artists, and venues" className="d-inline form-control w-25"/>
-                </InputGroup>
-                <button className="btn d-inline btn-dark mt-2">Search</button>
+                <h2 className="ms-2">Search Your Events</h2>
+                <div className="col ms-2 me-2">
+                    <InputGroup className="mt-2 mb-2">
+                        <InputGroup.Text>
+                            <i className="bi bi-geo-alt"></i>
+                        </InputGroup.Text>
+                        <input placeholder="City or Zip Code" className="d-inline form-control"/>
+                    </InputGroup>
+                    <InputGroup className="mb-2">
+                        <InputGroup.Text>
+                            <i className="bi bi-geo-alt"></i>
+                        </InputGroup.Text>
+                        <input placeholder="Search for events, artists, and venues" className="d-inline form-control w-25"/>
+                    </InputGroup>
+                </div>
+                <button className="btn d-inline btn-dark mt-2 ms-2 me-2">Search</button>
+
             </div>
         </section>
     );
