be07251d1ff7f5182905cb685a55b5366cb4a082
jaimesi <si.ja@northeastern.edu>
2023-04-13 14:25:12 -0400

    navbar works yay 2

--- src/nav.js
+++ src/nav.js
@@ -1,4 +1,4 @@
-import {NavLink, useLocation} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import {useSelector} from "react-redux";
 import React from "react";
 import "./eventory/index.css";
