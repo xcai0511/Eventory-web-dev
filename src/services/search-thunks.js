b71aa069ee96b6a2c55f96c3e2c2661ab526c3f1
Xiaojin Cai <xcai0511@gmail.com>
2023-04-14 10:01:17 -0400

    no result

--- src/services/search-thunks.js
+++ src/services/search-thunks.js
@@ -4,7 +4,7 @@ import * as service
     from "./search-service"
 
 export const searchThunk = createAsyncThunk(
-    'search/results', async ({rejectWithValue}) => {
+    'search/results', async ({ rejectWithValue }) => {
         try {
             const findResult = await service.findEventsInMa();
             return findResult;
@@ -12,13 +12,14 @@ export const searchThunk = createAsyncThunk(
             if (!e.response) {
                 return rejectWithValue('Something went wrong');
             }
+            return rejectWithValue(e.response.data);
         }
     }
 
 )
 
 export const searchFilterThunk = createAsyncThunk(
-    'search/searchFilter', async ({location, keyword}, {rejectWithValue}) => {
+    'search/searchFilter', async ({location, keyword}, { rejectWithValue }) => {
         try {
             const findResult = await service.findEventsByFilter(location, keyword);
             return findResult;
@@ -26,6 +27,7 @@ export const searchFilterThunk = createAsyncThunk(
             if (!e.response) {
                 return rejectWithValue('Something went wrong');
             }
+            return rejectWithValue(e.response.data);
         }
     }
 
