b70d2fff3586fc365cb93b51091c9af4ec67df30
jaimesi <si.ja@northeastern.edu>
2023-04-20 13:28:40 -0400

    changes 2 by xh

---
+++ src/reducers/organizerEvents-reducer.js
@@ -0,0 +1,36 @@
+// import { createSlice } from '@reduxjs/toolkit';
+// import {
+//     fetchEventsByOrganizerIdThunk,
+// } from '../services/organizerEvent-thunks';
+//
+// const initialState = {
+//     status: 'idle',
+//     events: null,
+//     error: null // string? json object?
+// };
+//
+// const eventsSlice = createSlice({
+//     name: 'events',
+//     initialState,
+//     reducers: {},
+//
+//     extraReducers: (builder) => {
+//         builder
+//             .addCase(fetchEventsByOrganizerIdThunk.pending, (state) => {
+//                 state.status = 'pending';
+//                 state.events = null;
+//                 state.error = null;
+//             })
+//             .addCase(fetchEventsByOrganizerIdThunk.fulfilled, (state, action) => {
+//                 state.status = 'fulfilled';
+//                 state.events = action.payload;
+//                 state.error = null;
+//             })
+//             .addCase(fetchEventsByOrganizerIdThunk.rejected, (state, action) => {
+//                 state.status = 'rejected';
+//                 state.events = null;
+//                 state.error = action.payload;
+//             })
+//     }});
+//
+// export default eventsSlice.reducer;
