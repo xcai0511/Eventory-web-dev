84d7eb6c3d6361eaedc6879f7907ac87a6831b7b
jaimesi <si.ja@northeastern.edu>
2023-04-23 13:04:43 -0400

    Fixing organizer-profile-details if statement bug

--- src/eventory/organizer-dashboard/index.js
+++ src/eventory/organizer-dashboard/index.js
@@ -21,32 +21,40 @@ const OrganizerDashboard = () => {
         dispatch(fetchEventsByOrganizerIdThunk(organizerId));
     }, [dispatch, organizerId]);
 
-    return (
-        <>
-            <div className="container mt-4">
-                <h4 className="mb-4">Welcome, {currentUser.name}!</h4>
-                <h5>Your Profile Information</h5>
+    if (currentUser) {
+        return (
+            <>
+                <div className="container mt-4">
+                    {(currentUser.role !== "organizer") ? <h3 className="mt-4 ms-4">Unauthorized.</h3> :
+                        <div>
+                            <h4 className="mb-4">Welcome, {currentUser.name}!</h4>
+                            <h5>Your Profile Information</h5>
 
-                <OrganizerProfileDetails />
+                            <OrganizerProfileDetails/>
 
-                <hr/>
+                            <hr/>
 
-                <div className="row mb-3">
-                    <div className="col text-end">
-                        <Link to="/create-event" className="btn btn-primary d-none d-md-inline-block me-4 mt-3 mb-3">
-                            Create Event
-                            <i className="bi bi-plus-lg ms-2"></i>
-                        </Link>
-                        <Link to="/create-event" className="btn btn-primary d-sm-inline-block d-md-none me-4 mt-3 mb-3">
-                            <i className="bi bi-plus-lg"></i>
-                        </Link>
-                    </div>
-                    {organizerId && <OrganizerEventsList organizerId={organizerId}/>}
+                            <div className="row mb-3">
+                                <div className="col text-end">
+                                    <Link to="/create-event"
+                                          className="btn btn-primary d-none d-md-inline-block me-4 mt-3 mb-3">
+                                        Create Event
+                                        <i className="bi bi-plus-lg ms-2"></i>
+                                    </Link>
+                                    <Link to="/create-event"
+                                          className="btn btn-primary d-sm-inline-block d-md-none me-4 mt-3 mb-3">
+                                        <i className="bi bi-plus-lg"></i>
+                                    </Link>
+                                </div>
+                                {organizerId && <OrganizerEventsList organizerId={organizerId}/>}
+                            </div>
+                        </div>
+                    }
                 </div>
-            </div>
-            <Footer />
-        </>
-    );
+                <Footer/>
+            </>
+        );
+    }
 };
 
 export default OrganizerDashboard;
--- src/eventory/organizer-dashboard/organizer-profile-details.js
+++ src/eventory/organizer-dashboard/organizer-profile-details.js
@@ -1,17 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {updateOrganizerByOrganizerIdThunk} from "../../services/organizers-thunks";
 import {profileThunk} from "../../services/auth-thunks";
-import {isCurrentUserRoleOrganizer} from "../../utils/utils";
 
 const OrganizerProfileDetails = () => {
 
     const dispatch = useDispatch();
     const currentUser = useSelector((state) => state.auth.currentUser);
-    isCurrentUserRoleOrganizer(currentUser);
 
-    const [name, setName] = useState(currentUser.name);
-    const [bio, setBio] = useState(currentUser.bio);
+    const [name, setName] = useState(currentUser?.name ?? '');
+    const [bio, setBio] = useState(currentUser?.bio ?? '');
 
     const [editable, setEditable] = useState(false);
 
@@ -40,41 +38,47 @@ const OrganizerProfileDetails = () => {
         setEditable(false);
     };
 
-    return (
-        <form>
-            <div className="form-group">
-                <label htmlFor="name" className="mb-1">Name</label>
-                <input
-                    type="text"
-                    className="form-control mb-3"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    disabled={!editable}
-                />
+    if (currentUser) {
+        return (
+            <div>
+                {(currentUser.role !== "organizer") ? <h3 className="mt-4 ms-4">Unauthorized.</h3> :
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="name" className="mb-1">Name</label>
+                            <input
+                                type="text"
+                                className="form-control mb-3"
+                                id="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                disabled={!editable}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="bio" className="mb-1">Organization Description</label>
+                            <input
+                                type="text"
+                                className="form-control mb-3"
+                                id="bio"
+                                value={bio}
+                                onChange={(e) => setBio(e.target.value)}
+                                disabled={!editable}
+                            />
+                        </div>
+                        {!editable ? (
+                            <button className="btn btn-primary" onClick={handleEditClick}>
+                                Edit Profile
+                            </button>
+                        ) : (
+                            <button className="btn btn-primary" onClick={handleSaveClick}>
+                                Save Profile
+                            </button>
+                        )}
+                    </form>
+                }
             </div>
-            <div className="form-group">
-                <label htmlFor="bio" className="mb-1">Organization Description</label>
-                <input
-                    type="text"
-                    className="form-control mb-3"
-                    id="bio"
-                    value={bio}
-                    onChange={(e) => setBio(e.target.value)}
-                    disabled={!editable}
-                />
-            </div>
-            {!editable ? (
-                <button className="btn btn-primary" onClick={handleEditClick}>
-                    Edit Profile
-                </button>
-            ) : (
-                <button className="btn btn-primary" onClick={handleSaveClick}>
-                    Save Profile
-                </button>
-            )}
-        </form>
-    );
+        );
+    }
 };
 
 export default OrganizerProfileDetails;
